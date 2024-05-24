e03c5c21281552f80d1ab9afc8bdbc5012d757d6
moon <313758455@qq.com>
2024-05-24 16:14:30 +0800

    fix InfiniteProxyCell hold data after UpdateContent

--- InfiniteProxyCell.ts
+++ InfiniteProxyCell.ts
@@ -5,6 +5,8 @@ const { ccclass } = _decorator;
 @ccclass('InfiniteProxyCell')
 export class InfiniteProxyCell extends InfiniteCell {
 
+    private data: any;
+
     /**
      * @en Asynchronously await the actual InfiniteCell and set it as a child of the current node
      * @zh 异步等待实际的 InfiniteCell，并将其设置为当前节点的子节点
@@ -12,13 +14,22 @@ export class InfiniteProxyCell extends InfiniteCell {
     public async awaitCell(it: Promise<InfiniteCell>) {
         const cell = await it;  // Wait for the Promise to return an InfiniteCell
         if (!this.isValid || !this.node.isValid || !cell) return;
+
         cell.node.setPosition(0, 0);
         cell.node.parent = this.node; // Set the actual cell node as a child of the current node
+
+        if (this.dataIndex >= 0 && this.data) {
+            this.UpdateContent(this.data); // Update the content of the actual cell node
+        }
     }
 
     UpdateContent(data: any): void {
+        if (!this.isValid || !this.node.isValid) return;
+        this.data = data;
+
         let node = this.node.children[0];
-        if (!this.isValid || !this.node.isValid || !node) return;
+        if (!node) return;
+
         let cell = node.getComponent('InfiniteCell') as InfiniteCell;
         cell && cell.UpdateContent(data); // Call the UpdateContent method of the InfiniteCell component on the child node to update content
     }
